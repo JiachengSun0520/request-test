c5d8e834869a20949acbce9bbb5e190a601baa7a
JiachengSun0520 <js211@rice.edu>
2022-06-03 10:38:09 -0500

    slider

--- src/App.js
+++ src/App.js
@@ -13,30 +13,54 @@ import { getValue } from '@testing-library/user-event/dist/utils';
 // const baseURL = 'https://voyages3-api.crc.rice.edu/voyage/'
 // const Token = 'Token 3e9ed2e0fa70a1a5cb6f34eb7a30ebde208ecd8f'
 
-const api = axios.create( {
-  baseURL: 'https://voyages3-api.crc.rice.edu/voyage/',
-  headers: {'Authorization': 'Token 0bfda2118118484d52aeec86812269aadeb37c67'},
-}) 
+var d = new FormData();
+d.append('aggregate_fields', ["voyage_slaves_numbers__imp_total_num_slaves_disembarked"]);
+
+const config =  {
+  method: 'post',
+  baseURL: 'https://voyages3-api.crc.rice.edu/voyage/aggregations',
+  headers: {'Authorization': 'Token 1bd7b6a695d87fb17a752fdcf58cc98c28486dd1'},
+  // data: {aggregate_fields: "voyage_slaves_numbers__imp_total_num_slaves_disembarked",}
+  data:d
+}
+
 
 
 function App() {
 
+
+  const [range, setRange] = React.useState([0,0])
+  React.useEffect(() => {
+    // axios(config).then(res => console.log(Object.values(res.data)[0]['max']))
+    // axios(config).then(res => (setRange(Object.values(res.data))["min"], Object.values(res.data)["max"]));
+    axios(config).then(res => (setRange([Object.values(res.data)[0]['min'], Object.values(res.data)[0]['max']])));
+    
+  }, [])
   // slider {
+  
+  console.log(range)
+  const [value, setValue] = React.useState([range[0]/2, range[1]/2])
+  const [dataSend, setDataSend] = React.useState(value)
+  
 
-  const [value, setValue] = React.useState([20,37])
-
-
-  function valuetext(value) {
-    console.log(`${value}°C`);
-    api.post("").then(res=> {return res.data})
-    // return `${value}°C`;
-
+  function handleCommittedChange() {
+    // console.log(`${value}°C`);
+    console.log("onchange", value);
+    // setDataSend(value);
+    console.log("dataSend", dataSend);
+    console.log('range', range);
+    // axios(config).then(res => setRange([range.min, range.max]))
+    // console.log(range)
+    
+    
   }
 
   // const [value, setValue] = React.useState([20,37])
   
   const handleChange = (event, newValue) => {
       setValue(newValue); 
+      // console.log("onchange", value);
+      setDataSend(newValue);
   };
   // } slider end
 
@@ -49,11 +73,14 @@ function App() {
               <header className="App-header">   
                   <Box sx={{ width: 300 }}>
                   <Slider
+                    min = {range[0]}
+                    max = {range[1]}
                     getAriaLabel={() => 'Temperature range'}
                     value={value}
+                    step={1}
                     onChange={handleChange}
+                    onChangeCommitted={handleCommittedChange}
                     valueLabelDisplay="auto"
-                    getAriaValueText={valuetext}
                   />
                 </Box>
                </header>
